fd91c2493bef426fbe955a1b5d9a9a6da8330b58
MInhTais <minnhtai2019cb2@gmail.com>
2024-09-09 15:54:59 +0700

    Update greetingService.ts

--- src/app/services/greeting/greetingService.ts
+++ src/app/services/greeting/greetingService.ts
@@ -14,71 +14,42 @@ const client = new GreeterClient('http://localhost:8000')
 export const greetingService = {
     getAll: async (): Promise<GreetingReply[]> => {
         const request = new ReadGreetingsRequest()
-        return new Promise((resolve, reject) => {
-            client.readGreetings(request, {}, (err, response) => {
-                if (err) {
-                    reject(err)
-                } else {
-                    resolve(response.getGreetingsList())
-                }
-            })
-        })
+
+        const response = await client.readGreetings(request, {})
+
+        return response.getGreetingsList()
     },
 
     getById: async (id: string): Promise<GreetingReply> => {
         const request = new GetGreetingByIdRequest()
         request.setId(id)
-        return new Promise((resolve, reject) => {
-            client.getGreetingById(request, {}, (err, response) => {
-                if (err) {
-                    reject(err)
-                } else {
-                    resolve(response)
-                }
-            })
-        })
+        const response = await client.getGreetingById(request, {})
+
+        return response
     },
 
     create: async (body: { name: string }): Promise<GreetingReply> => {
         const request = new CreateGreetingRequest()
         request.setName(body.name)
-        return new Promise((resolve, reject) => {
-            client.createGreeting(request, {}, (err, response) => {
-                if (err) {
-                    reject(err)
-                } else {
-                    resolve(response)
-                }
-            })
-        })
+        const response = await client.createGreeting(request, {})
+
+        return response
     },
 
     delete: async (id: string): Promise<DeleteGreetingReply> => {
         const request = new DeleteGreetingRequest()
         request.setId(id)
-        return new Promise((resolve, reject) => {
-            client.deleteGreeting(request, {}, (err, response) => {
-                if (err) {
-                    reject(err)
-                } else {
-                    resolve(response)
-                }
-            })
-        })
+        const response = await client.deleteGreeting(request, {})
+
+        return response
     },
 
     update: async (id: string, body: { name: string }): Promise<GreetingReply> => {
         const request = new UpdateGreetingRequest()
         request.setId(id)
         request.setName(body.name)
-        return new Promise((resolve, reject) => {
-            client.updateGreeting(request, {}, (err, response) => {
-                if (err) {
-                    reject(err)
-                } else {
-                    resolve(response)
-                }
-            })
-        })
+        const response = await client.updateGreeting(request, {})
+
+        return response
     }
 }
--- src/app/services/greeting/protos/cmGen.sh
+++ src/app/services/greeting/protos/cmGen.sh
@@ -2,3 +2,16 @@ protoc -I=./src/app/protos ./src/app/protos/greeting.proto \
   --plugin=protoc-gen-grpc-web=./node_modules/.bin/protoc-gen-grpc-web \
   --js_out=import_style=commonjs:./src/app/services/greeting/protos \
   --grpc-web_out=import_style=typescript,mode=grpcwebtext:./src/app/services/greeting/protos
+
+# vs
+# protoc -I=./src/app/protos ./src/app/protos/greeting.proto \
+#   --js_out=import_style=commonjs,binary:./src/app/services/test/protos \
+#   --grpc-web_out=import_style=typescript,mode=grpcwebtext:./src/app/services/test/protos
+
+
+# gen 
+# protoc -I=./src/app/protos ./src/app/protos/greeting.proto \
+#   --js_out=import_style=commonjs:./src/app/services/test/protos \
+#   --grpc-web_out=import_style=typescript,mode=grpcwebtext:./src/app/services/test/protos
+
+#test
